b193d627bda1a87c443fb3ec9f20e01ad6e75dfe
DanteTP <dante_tachella@hotmail.com>
2021-01-28 19:32:24 -0300

    Creacion contenedor ItemListContainer y montaje del mismo en la App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css'
 import NavBar from './containers/NavBar';
+import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer mensaje={"Bienvenidos a DTCOMMERCE!!"}/>
     </div>
   );
 }
---
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+const ItemListContainer = (props) =>{
+    return (
+        <h1>{props.mensaje}</h1>
+
+    )
+}
+
+export default ItemListContainer
