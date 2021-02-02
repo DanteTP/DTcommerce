eb3a762a71b6419eba4860d21377a50bdff4c097
DanteTP <dante_tachella@hotmail.com>
2021-02-02 16:52:03 -0300

    Creacion boton ItemCoun e incorporacion a ItemListContainer / creacion de hooks para agregar a carrito y conteo de cantidad de items

--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,21 @@
 import './style.css'
+import React, { useState } from 'react'
+import ItemCount from '../../components/ItemCount'
+
+
 
 const ItemListContainer = (props) =>{
-    return (
-        <h1>{props.mensaje}</h1>
 
+    const [cart,SetCart] = useState(0)
+
+    const handlerAdd= (stock,addvalue)=>{
+        if(addvalue<=(stock-cart)){SetCart(cart+addvalue)}else{alert("ingrese una cantidad menor ya que no tenemos suficiente stock")}}
+
+    return (
+        <>
+        <h1>{props.mensaje}</h1>
+        <ItemCount stock={6} initial={1} handlerAdd={handlerAdd} cart={cart}/>
+        </>
     )
 }
 
